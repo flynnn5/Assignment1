4e18cbf86a4fc8c9ee410ff601ebd08cc9e89e7a
flynnn5 <32555466+flynnn5@users.noreply.github.com>
2017-10-14 11:46:10 +0100

    Bones of BT

--- BinaryTree.java
+++ BinaryTree.java
@@ -1,9 +1,27 @@
+import java.util.NoSuchElementException;
 
-public class BinaryTree {
+public class BinaryTree<Key extends Comparable<Key>> {
+	private Node root;
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	private class Node {
+		private Key key;
+		private Node left, right;
 
+		public Node(Key key) {
+			this.key = key;
+		}
 	}
 
+	private Node put(Node x, Key key) {
+
+		return null;
+	}
+
+	private Node findLCA() {
+		return root;
+	}
+
+	public BinaryTree() {
+
+	}
 }
